46fa981b8534076c3315acc636bc0dd100344ea0
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-06-06 21:22:35 -0300

    Feita o css do componente Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,19 @@
-import './Main'
+import './Main.css'
 
 function Main() {
-    return(
+    return (
         <main className='mainPrincipal'>
-            <div>
-                <p>Minha Página</p>
+            <div className='mainSections'>
+                <div className='divSobreMim'>
+                    <section className='sectionSobreMin'>
+                        <p>Sobre mim!</p>
+                        <p>Estou envolvido em todas as etapas do ciclo de vida do desenvolvimento de software, utilizando tecnologias como .NET, C#, React e SQL para criar soluções inovadoras e eficazes.</p>
+                    </section>
+                </div>
+                <div className='divProjetos'>
+                    <section><p>Meus Projetos</p></section>
+                </div>
+
             </div>
         </main>
     )
